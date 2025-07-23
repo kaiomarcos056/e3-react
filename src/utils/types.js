0065fdd8b311a9b9956d6378942f27327e048c90
Marcos Kaio <kaionery111@gmail.com>
2025-07-22 23:52:31 -0300

    adjusting mapping

--- src/utils/types.js
+++ src/utils/types.js
@@ -6,19 +6,19 @@ export const TileValues = {
 export const CategoryValues = {
     // FLOOR: "floor",
     // WALL: "wall",
-    // DOOR_WINDOW: "doorwindow",
+    DOOR_WINDOW: "doorwindow",
     // FURNITURE: "furniture",
     // ELECTRONICS: "electronics",
     // UTENSILS: "utensils",
-    // GOALS: "goals",
+    GOALS: "goals",
     // PLAYER: "persons",
 
     FLOOR: "floor",
     WALL: "walls",
-    DOOR_WINDOW: "door_and_windows",
+    // DOOR_WINDOW: "door_and_windows",
     FURNITURE: "furniture",
     ELECTRONICS: "eletronics",
     UTENSILS: "utensils",
-    GOALS: "interactive_elements",
+    // GOALS: "interactive_elements",
     PLAYER: "persons",
 };
